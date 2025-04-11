0d9b28a265baa469b1582ff83af94e12041f3d96
Eman Tariq <emantariq.1004@gmail.com>
2025-04-11 21:31:31 +0500

    updating title

--- components/Document.tsx
+++ components/Document.tsx
@@ -1,31 +1,52 @@
 "use client"
 
-import { useState } from "react"
+import { useState, useTransition } from "react"
 import { Input } from "./ui/input"
+import { Button } from "./ui/button"
+import { doc, updateDoc } from "firebase/firestore"
+import { db } from "@/firebase"
 
 
 function Document({ id }: { id: string }) {
     const [input, setInput] = useState("")
+    const [isUpdating, startTransition] = useTransition()
+
+    const updateTitle = (e: FormEvent) => {
+        e.preventDefault()
+
+        if (input.trim()) {
+            startTransition(async () => {
+                await updateDoc(doc(db, "documents", id), {
+                    title: input,
+                })
+            })
+        }
+    }
 
 
-    return (
-        <div>
+
+        return (
             <div>
-                <form>
-                    update title
-                    <Input value={input} onChange={(e) => setInput(e.target.value)} />
-                    {/* if */}
-                    {/* isOwner + invite + deleteDocument */}
-                </form>
-            </div>
+                <div>
+                    <form onSubmit={updateTitle}>
+                        {/* update title */}
+                        <Input value={input} onChange={(e) => setInput(e.target.value)} />
 
-            <div>
-                {/* ManageUser  */}
-                {/* Avatars  */}
-            </div>
+                        <Button disabled={isUpdating} type="submit">
+                            {isUpdating ? "Updating..." : "Update"}
+                        </Button>
+                        {/* if */}
+                        {/* isOwner + invite + deleteDocument */}
+                    </form>
+                </div>
 
-            {/* Collaberative editor  */}
-        </div>
-    )
-}
+                <div>
+                    {/* ManageUser  */}
+                    {/* Avatars  */}
+                </div>
+
+                {/* Collaberative editor  */}
+            </div>
+        )
+    }
 export default Document
