6e3c0f00e59dade73561727e8ada11532299b879
Eman Tariq <emantariq.1004@gmail.com>
2025-04-13 14:41:37 +0500

    fixed a bug

--- components/Document.tsx
+++ components/Document.tsx
@@ -1,15 +1,24 @@
 "use client"
 
-import { useState, useTransition } from "react"
+import { useState, useTransition, useEffect, FormEvent } from "react"
 import { Input } from "./ui/input"
 import { Button } from "./ui/button"
 import { doc, updateDoc } from "firebase/firestore"
 import { db } from "@/firebase"
+import { useDocumentData } from "react-firebase-hooks/firestore"
 
 
 function Document({ id }: { id: string }) {
+    const [data, loading, error] = useDocumentData(doc(db, "documents", id));
     const [input, setInput] = useState("")
     const [isUpdating, startTransition] = useTransition()
+
+    useEffect(() => {
+        if (data) {
+            setInput(data.title)
+        }
+    }, [data])
+
 
     const updateTitle = (e: FormEvent) => {
         e.preventDefault()
@@ -25,28 +34,28 @@ function Document({ id }: { id: string }) {
 
 
 
-        return (
+    return (
+        <div>
             <div>
-                <div>
-                    <form onSubmit={updateTitle}>
-                        {/* update title */}
-                        <Input value={input} onChange={(e) => setInput(e.target.value)} />
+                <form onSubmit={updateTitle}>
+                    {/* update title */}
+                    <Input value={input} onChange={(e) => setInput(e.target.value)} />
 
-                        <Button disabled={isUpdating} type="submit">
-                            {isUpdating ? "Updating..." : "Update"}
-                        </Button>
-                        {/* if */}
-                        {/* isOwner + invite + deleteDocument */}
-                    </form>
-                </div>
-
-                <div>
-                    {/* ManageUser  */}
-                    {/* Avatars  */}
-                </div>
-
-                {/* Collaberative editor  */}
+                    <Button disabled={isUpdating} type="submit">
+                        {isUpdating ? "Updating..." : "Update"}
+                    </Button>
+                    {/* if */}
+                    {/* isOwner + invite + deleteDocument */}
+                </form>
             </div>
-        )
-    }
+
+            <div>
+                {/* ManageUser  */}
+                {/* Avatars  */}
+            </div>
+
+            {/* Collaberative editor  */}
+        </div>
+    )
+}
 export default Document
