20b1ab0deac7233b57d7dea74e5bd74e18e85035
Eman Tariq <emantariq.1004@gmail.com>
2025-04-20 21:15:50 +0500

    avatars

--- components/Avatars.tsx
+++ components/Avatars.tsx
@@ -1,24 +1,44 @@
-"use client"
+"use client";
 
-import { useOthers, useSelf } from "@liveblocks/react/suspense"
-
+import { useOthers, useSelf } from "@liveblocks/react/suspense";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 function Avatars() {
-    const others = useOthers();
-    const self = useSelf();
+  const others = useOthers();
+  const self = useSelf();
 
-    const all = [self, ...others];
+  const all = [self, ...others];
   return (
     <div className="flex items-center gap-2">
-        <p className="font-light text-sm">Users currently editing this page</p>
+      <p className="font-light text-sm">Users currently editing this page</p>
 
-        <div className="flex -space-x-5">
-            {all.map((other, i) => (
-
-            ))}
-        </div>
-        
+      <div className="flex -space-x-5">
+        {all.map((other, i) => (
+          <TooltipProvider key={other?.id + i}>
+            <Tooltip>
+              <TooltipTrigger>
+                <Avatar className="border-2 hover:z-50">
+                  <AvatarImage src={other?.info.avatar} alt={other?.info?.name} />
+                  <AvatarFallback>{other?.info.name}</AvatarFallback>
+                </Avatar>
+              </TooltipTrigger>
+              <TooltipContent>
+              <Avatar className="border-2 hover:z-50">
+                  <AvatarImage src={other?.info.avatar} alt={other?.info?.name} />
+                  <AvatarFallback>{other?.info.name}</AvatarFallback>
+                </Avatar>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
-export default Avatars
+export default Avatars;
